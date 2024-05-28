7050480005c92d6e711292a60d50b3ac3bba2493
Jacob <jacobjokey@gmail.com>
2024-05-29 00:38:24 +0700

    auth page responsive

--- frontend/src/components/misc/StraightTrough.jsx
+++ frontend/src/components/misc/StraightTrough.jsx
@@ -2,7 +2,7 @@ function StraightTrough({label}) {
 return (
     <div className="flex justify-center">
         <div className="w-full">
-            <div className="relative flex items-center py-5">
+            <div className="relative flex items-center py-2 md:py-5">
                 <div className="flex-grow border-t border-gray-500"></div>
                 <span className="flex-shrink mx-4 font-medium text-gray-600">{label}</span>
                 <div className="flex-grow border-t border-gray-500"></div>
--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,36 +1,110 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import AuthButton from "../../components/misc/AuthButton";
+import SocialButton from "../../components/misc/SocialButton";
+import StraightTrough from "../../components/misc/StraightTrough";
 
 function Register() {
-    return (
-        <div className='grid h-screen grid-cols-2'>
-            <div className='bg-g1 flex'>
-                <img className='mx-auto self-center' src="/src/assets/img/logo.png" alt="" />
+  return (
+    <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
+      <div className="flex bg-g1">
+        <img
+          className="self-center w-1/2 h-auto mx-auto"
+          src="/src/assets/img/logo.png"
+          alt="Logo"
+        />
+      </div>
+      <div className="flex flex-col justify-center px-10 space-y-2 bg-white md:p-32 md:space-y-4">
+        <h1 className="mb-3 text-3xl font-bold md:mb-5 md:text-4xl">Daftar</h1>
+        <form className="w-full max-w-md mx-auto">
+          <label
+            htmlFor="email-address-icon"
+            className="block mb-1 font-medium md:mb-2">
+            Username
+          </label>
+          <div className="relative mb-3">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <span className="text-gray-400 material-symbols-outlined">
+                person
+              </span>
             </div>
-            <div className='self-center p-36 bg-white'>
-                <h1 className='mb-10 text-4xl font-bold'>Daftar</h1>
-                <form class="mx-auto">
-                    <label for="email-address-icon" class="block mb-2 text-sm font-medium">Email</label>
-                    <div class="relative mb-3">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <span class="material-symbols-outlined text-gray-400">mail</span>
-                        </div>
-                        <input type="text" id="email-address-icon" class="border-c1b border-2 w-full p-2 block ps-12" placeholder="Masukkan Email" />
-                    </div>
-                    <label for="email-address-icon" class="block mb-2 text-sm font-medium">Kata Sandi</label>
-                    <div class="relative mb-3">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <span class="material-symbols-outlined text-gray-400">lock</span>
-                        </div>
-                        <input type="text" id="email-address-icon" class="border-c1b border-2 w-full p-2 block ps-12" placeholder="Masukkan Password" />
-                    </div>
-                    <button className='w-full p-4 text-lg font-normal text-center text-white rounded-sm h-14 bg-secondary'>Daftar</button>
-                </form>
-                <h1 className='my-5 font-medium text-right'>Sudah punya akun? <a className='text-primary' href="">Masuk</a></h1>
-                <h1 className='my-5 font-medium'>Atau daftar dengan Google<hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr></h1>
-                <button className='w-full p-4 text-lg font-normal text-center border-2 h-14 border-c1b'>Daftar dengan Google</button>
+            <input
+              type="text"
+              id="email-address-icon"
+              className="block w-full p-2 md:p-3.5 pl-12 border-2 border-c1b"
+              placeholder="username"
+            />
+          </div>
+          <label
+            htmlFor="email-address-icon"
+            className="block mb-1 font-medium md:mb-2">
+            Email
+          </label>
+          <div className="relative mb-3">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <span className="text-gray-400 material-symbols-outlined">
+                mail
+              </span>
             </div>
-        </div>
-    )
+            <input
+              type="text"
+              id="email-address-icon"
+              className="block w-full p-2 md:p-3.5 pl-12 border-2 border-c1b"
+              placeholder="email@email.com"
+            />
+          </div>
+          <label
+            htmlFor="password-icon"
+            className="block mb-1 font-medium md:mb-2">
+            Kata Sandi
+          </label>
+          <div className="relative mb-3">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <span className="text-gray-400 material-symbols-outlined">
+                lock
+              </span>
+            </div>
+            <input
+              type="password"
+              id="password-icon"
+              className="block w-full p-2 md:p-3.5 pl-12 border-2 border-c1b"
+              placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
+            />
+          </div>
+          <label
+            htmlFor="password-icon"
+            className="block mb-1 font-medium md:mb-2">
+            Konfirmasi Kata Sandi
+          </label>
+          <div className="relative mb-5">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <span className="text-gray-400 material-symbols-outlined">
+                lock
+              </span>
+            </div>
+            <input
+              type="password"
+              id="password-icon"
+              className="block w-full p-2 md:p-3.5 pl-12 border-2 border-c1b"
+              placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
+            />
+          </div>
+          <AuthButton link="/register" label="Daftar" />
+        </form>
+        <p className="text-sm">
+          Mempunyai akun?{" "}
+          <Link className="text-primary" to="/login">
+            Masuk Disini
+          </Link>
+        </p>
+        <StraightTrough label="Atau daftar menggunakan" />
+        <SocialButton
+          icon="/src/assets/icons/google.svg"
+          label="Daftar dengan Google"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
